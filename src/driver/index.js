664eda6181901c1f7b11ea06243f920340eee3f2
PROVIDA POWELL <PROVIDA_POWELL@YAHOO.COM>
2023-01-24 01:38:25 +0000

    demo code updates complete; need to write tests then resubmit

---
+++ src/driver/index.js
@@ -0,0 +1,31 @@
+const { EVENT_NAMES, chance } = require('../utils');
+const { io } = require("socket.io-client");
+
+const events = io("ws://localhost:3333");  //protocol and domain/hostname and port
+
+function deliver(orderId) {
+  console.log("Driver finished delivery of orderId: ", orderId);
+  events.emit(EVENT_NAMES.delivered, orderId);
+}
+
+function handlePickup(event) {
+  console.log("Driver recieved a pickup event!", event);
+  setTimeout(
+    () => deliver(event.orderId),
+    chance.integer({ min: 500, max: 1000 })
+  );
+}
+
+function startDriver() {
+  console.log("Driver ready!");
+
+  events.on(EVENT_NAMES.pickup, handlePickup);
+}
+
+module.exports = {
+ toTest: {
+    deliver,
+    handlePickup,
+  }
+};
+startDriver();
--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,10 @@
 // this is what we're going to replace with socket.io in lab 12
 
-const { EventEmitter } = require('events');
 const chance = require("chance")();
-
-const events = new EventEmitter();
 
 const EVENT_NAMES = {
   pickup: "pickup",
   delivered: "delivered orderID:",
 }
 
-module.exports = { events, chance, EVENT_NAMES };
+module.exports = { chance, EVENT_NAMES };
